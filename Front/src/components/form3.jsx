23610c1eb34732ae9fdf6140ee6244f053cab661
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2024-03-30 17:24:15 -0400

    gitignore

--- Front/src/components/form3.jsx
+++ Front/src/components/form3.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
-import './form1.css';
+import React from "react";
+import { useState } from "react";
+import "./form1.css";
+import axios from "axios";
 
 function form3() {
-    return (
-        <div className = 'sign-up-container'> 
-            <input type = 'text' placeholder = 'Skills'/>
-            <input type = 'text' placeholder = 'GitHub Username'/>
-        </div>
-    )
+  let [ghUsername, setGhUsername] = useState("");
+  const submit = () => {
+    axios
+      .get(`http://localhost:8000/getprojects?username=${ghUsername}`)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="sign-up-container">
+      <input type="text" placeholder="Skills" />
+      <input
+        type="text"
+        placeholder="GitHub Username"
+        value={ghUsername}
+        onChange={(e) => setGhUsername(e.target.value)}
+      />
+      <button onClick={submit}>Submit</button>
+    </div>
+  );
 }
 
 export default form3;
