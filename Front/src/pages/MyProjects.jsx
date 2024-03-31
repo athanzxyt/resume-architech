0f45f23715d38ec497ada2af5cefddb5bea7feb1
Timur Gubskiy <34976482+Tim-gubski@users.noreply.github.com>
2024-03-31 00:46:27 -0400

    delete

--- Front/src/pages/MyProjects.jsx
+++ Front/src/pages/MyProjects.jsx
@@ -45,21 +45,23 @@ export default function MyProjects() {
       });
   };
 
-// delteProject function
-    const deleteProject = (project) => {
-        axios
-        .post("http://localhost:8000/deleteproject", {
-            username: window.localStorage.getItem("username"),
-            project: project,
-        })
-        .then((res) => {
-            console.log(res.data);
-            setProjects(res.data.repos);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    };
+  // delteProject function
+  const deleteProject = (project) => {
+    let newProjects = { ...projects };
+    delete newProjects[project];
+    axios
+      .post("http://localhost:8000/updateprojects", {
+        username: window.localStorage.getItem("username"),
+        repos: newProjects,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setProjects(res.data.repos);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const generate = () => {
     axios
@@ -78,6 +80,7 @@ export default function MyProjects() {
 
   return (
     <div className="grid grid-cols-2 gap-4 p-8">
+<<<<<<< Updated upstream
         <div className="flex flex-col p-8">
             <h1 className='text-3xl font-bold pb-4'>Add Additional Projects</h1>
             <input
@@ -108,7 +111,58 @@ export default function MyProjects() {
                     ))}
                 </ul>
             </div>
+=======
+      <div className="flex flex-col p-8">
+        <h1 className="text-3xl font-bold pb-4">Add Additional Projects</h1>
+        <input
+          className="border-2 rounded-md p-2 mb-4"
+          type="text"
+          placeholder="Project Name"
+          value={projectName}
+          onChange={(e) => setProjectName(e.target.value)}
+        />
+        <textarea
+          className="h-64 border-2 rounded-md p-2 mb-4"
+          placeholder="Project Description"
+          value={projectDescription}
+          onChange={(e) => setProjectDescription(e.target.value)}
+        />
+        <button
+          className="w-1/2 p-2 border-2 rounded-md mb-8"
+          onClick={addProject}
+        >
+          Add
+        </button>
+        <button
+          className="w-1/2 p-2 border-2 rounded-md bg-black text-white"
+          onClick={generate}
+        >
+          Generate Bullets
+        </button>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="flex justify-center w-full">
+          <ul className="w-full p-8">
+            <h1 className="text-3xl font-bold pb-4">Current Projects</h1>
+            {Object.values(projects).map((project, index) => (
+              <li className="flex justify-between w-full border-2 rounded-md p-2 m-2">
+                <span>
+                  {index + 1}: {project.name}
+                </span>
+                <span
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    deleteProject(project.name);
+                  }}
+                >
+                  Delete
+                </span>
+              </li>
+            ))}
+          </ul>
+>>>>>>> Stashed changes
         </div>
+      </div>
     </div>
   );
 }
